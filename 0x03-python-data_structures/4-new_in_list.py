8356a171be6d4056d3c68dbbc81970386689678d
BOUTCHOUANG1 <101360639+BOUTCHOUANG1@users.noreply.github.com>
2022-07-04 19:30:53 +0100

    Update 4-new_in_list.py
    4. Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,8 @@
-xxnxjj
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if not my_list:
+        return
+    new_list = my_list.copy()
+    if 0 <= idx < len(new_list):
+        new_list[idx] = element
+    return new_list
