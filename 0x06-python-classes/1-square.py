fcf68b269b0d54d9ef5dd361ecaee4c57e112b17
BOUTCHOUANG1 <101360639+BOUTCHOUANG1@users.noreply.github.com>
2022-07-21 06:36:05 +0100

    Update 1-square.py
    1. Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,11 @@
-hjd
+#!/usr/bin/python3
+"""
+This module defines a Square class and initialise its size
+"""
+
+
+class Square:
+    """Square implementation
+    """
+    def __init__(self, size):
+        self.__size = size
